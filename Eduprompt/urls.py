461a503da5b0f3feb293172fc0648381ae12b7ab
YOUR_GITHUB_USERNAME <prospereze12345@gmail.com>
2025-09-02 22:58:56 +0100

    Your message

--- Eduprompt/urls.py
+++ Eduprompt/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('', views.index, name='index'),  
     path('contact/', views.contact, name='contact'), 
     path('about/', views.about, name='about'), 
-    path('Terms-and-Condition/', views.Terms_and_Condition, name='Terms-and-Condition'), 
+    path('Terms_and_Condition/', views.Terms_and_Condition, name='Terms_and_Condition'), 
     path('privacy_policy/', views.privacy_policy, name='privacy_policy'), 
    
 
