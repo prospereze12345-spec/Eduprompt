b7728cad632e1eb29ec42d896aae2f734d575fa8
YOUR_GITHUB_USERNAME <prospereze12345@gmail.com>
2025-10-04 21:38:36 +0100

    Your message

--- new_project/settings.py
+++ new_project/settings.py
@@ -279,13 +279,6 @@ LOGIN_REDIRECT_URL = "/"          # After login
 
 
 
-EMAIL_BACKEND = "sendgrid_backend.SendgridBackend"
-SENDGRID_API_KEY = os.environ.get("SENDGRID_API_KEY")
-DEFAULT_FROM_EMAIL = "eduprompt@outlook.com"
-
-SENDGRID_SANDBOX_MODE_IN_DEBUG = False
-SENDGRID_ECHO_TO_STDOUT = True
-
 
 EMAIL_BACKEND = "sendgrid_backend.SendgridBackend"
 SENDGRID_API_KEY = os.environ.get("SENDGRID_API_KEY")
